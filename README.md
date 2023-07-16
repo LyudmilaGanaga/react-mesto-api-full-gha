acdf1e66f42c3770c77e9004a213b94eeb0bf55e
Mila <milanaganaga@yandex.ru>
2023-07-16 11:01:41 +0400

    Проект 15

--- README.md
+++ README.md
@@ -5,12 +5,12 @@
   
 Пожалуйста, прикрепите в это описание ссылку на сайт, размещенный на Яндекс.Облаке.
 
-Адрес репозитория: https://github.com/...
+Адрес репозитория: https://github.com/LyudmilaGanaga/react-mesto-api-full-gha.git
 
 ## Ссылки на проект
 
-IP-адрес x.x.x.x
+IP 158.160.16.168
 
-Frontend https://...
+Frontend https://pr-mesto.nomoredomains.work
 
-Backend https://...
+Backend https://api.pr-mesto.nomoredomains.xyz
--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
+
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const routes = require('./routes');
+const { requestLogger, errorLogger } = require('./middlwares/logger');
 const errorHandler = require('./middlwares/error');
 
 const { PORT = 3000 } = process.env;
@@ -11,8 +14,20 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 const app = express();
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(express.json());
 app.use(routes);
+
+app.use(errorLogger);
+
 app.use(errors());
 app.use(errorHandler);
 
