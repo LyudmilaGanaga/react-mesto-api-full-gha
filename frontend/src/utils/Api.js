2d51683586d117a50b337b395ea48021923cf3f8
Mila <milanaganaga@yandex.ru>
2023-07-17 00:50:19 +0400

    для ВМ

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,7 +1,8 @@
+
 class Api {
-  constructor(config) {
-    this._url = config.url;
-    this._headers = config.headers;
+  constructor({ url, headers }) {
+    this._url = url;
+    this._headers = headers;
   }
 
   // проверка
@@ -12,46 +13,72 @@ class Api {
       return Promise.reject(`Ошибка: ${res.status}`);
     }
   }
+
+  _setHeaders () {
+    const token = localStorage.getItem('token');
+    return {
+      'Authorization': `Bearer ${token}`,
+      ...this._headers,
+    };
+  }
+  
   // получить список всех карточек в виде массива (GET)
   getInitialCards() {
     return fetch(`${this._url}/cards`, {
-      method: "GET",
-      headers: this._headers,
+      // method: "GET",
+      headers: this._setHeaders(),
     }).then(this._getJson);
   }
+
+// получить данные пользователя (GET)
+getDataUser() {
+  return fetch(`${this._url}/users/me`, {
+    // method: "GET",
+    headers: this._setHeaders(),
+  }).then(this._getJson);
+}
 
   // добавить карточку (POST)
   addCard(data) {
     return fetch(`${this._url}/cards`, {
       method: "POST",
-      headers: this._headers,
+      headers: this._setHeaders(),
       body: JSON.stringify({
         name: data.name,
         link: data.link,
       }),
     }).then(this._getJson);
   }
+
   // удалить карточку (DELETE)
   deleteCard(id) {
     return fetch(`${this._url}/cards/${id}`, {
       method: "DELETE",
-      headers: this._headers,
+      headers: this._setHeaders(),
     }).then(this._getJson);
   }
 
-  // получить данные пользователя (GET)
-  getDataUser() {
-    return fetch(`${this._url}/users/me`, {
-      // method: "GET",
-      headers: this._headers,
+  // “залайкать” карточку (PUT)
+  putLike(id) {
+    return fetch(`${this._url}/cards/${id}/likes`, {
+      method: "PUT",
+      headers: this._setHeaders(),
     }).then(this._getJson);
   }
+
+    // удалить лайк карточки (DELETE)
+    deleteLike(id) {
+      return fetch(`${this._url}/cards/${id}/likes`, {
+        method: "DELETE",
+        headers: this._setHeaders(),
+      }).then(this._getJson);
+    }
 
   // заменить данные пользователя (PATCH)
   editInfoUser(data) {
     return fetch(`${this._url}/users/me`, {
       method: "PATCH",
-      headers: this._headers,
+      headers: this._setHeaders(),
       body: JSON.stringify({
         name: data.name,
         about: data.about,
@@ -63,28 +90,13 @@ class Api {
   editUserAvatar(data) {
     return fetch(`${this._url}/users/me/avatar`, {
       method: "PATCH",
-      headers: this._headers,
+      headers: this._setHeaders(),
       body: JSON.stringify({
         avatar: data.avatar,
       }),
     }).then(this._getJson);
   }
 
-  // “залайкать” карточку (PUT)
-  putLike(id) {
-    return fetch(`${this._url}/cards/${id}/likes`, {
-      method: "PUT",
-      headers: this._headers,
-    }).then(this._getJson);
-  }
-
-  // удалить лайк карточки (DELETE)
-  deleteLike(id) {
-    return fetch(`${this._url}/cards/${id}/likes`, {
-      method: "DELETE",
-      headers: this._headers,
-    }).then(this._getJson);
-  }
 
   // для лайка
   changeLikeCardStatus(cardId, isLiked) {
@@ -96,28 +108,27 @@ class Api {
   }
 }
 
-// export const api = new Api({
-//   url: 'https://api.pr-mesto.nomoredomains.xyz',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     authorization: `Bearer ${localStorage.getItem('jwt')}`,
-//   },
-// });
-
 export const api = new Api({
-  url: "https://mesto.nomoreparties.co/v1/cohort-62",
+  url: 'https://api.pr-mesto.nomoredomains.xyz',
   headers: {
-    "Content-Type": "application/json",
-    authorization: "fff1efa7-9818-44da-ba96-913e90767349",
+    'Content-Type': 'application/json',
+    authorization: `Bearer ${localStorage.getItem('jwt')}`,
   },
 });
 
+// export const api = new Api({
+//   url: "https://mesto.nomoreparties.co/v1/cohort-62",
+//   headers: {
+//     "Content-Type": "application/json",
+//     authorization: "fff1efa7-9818-44da-ba96-913e90767349",
+//   },
+// });
 
 
 // class Api {
-//   constructor({ url, headers }) {
-//     this._url = url;
-//     this._headers = headers;
+//   constructor(config) {
+//     this._url = config.url;
+//     this._headers = config.headers;
 //   }
 
 //   // проверка
@@ -128,72 +139,46 @@ export const api = new Api({
 //       return Promise.reject(`Ошибка: ${res.status}`);
 //     }
 //   }
-
-//   _setHeaders () {
-//     const token = localStorage.getItem('token');
-//     return {
-//       'Authorization': `Bearer ${token}`,
-//       ...this._headers,
-//     };
-//   }
-  
 //   // получить список всех карточек в виде массива (GET)
 //   getInitialCards() {
 //     return fetch(`${this._url}/cards`, {
-//       // method: "GET",
-//       headers: this._setHeaders(),
+//       method: "GET",
+//       headers: this._headers,
 //     }).then(this._getJson);
 //   }
-
-// // получить данные пользователя (GET)
-// getDataUser() {
-//   return fetch(`${this._url}/users/me`, {
-//     // method: "GET",
-//     headers: this._setHeaders(),
-//   }).then(this._getJson);
-// }
 
 //   // добавить карточку (POST)
 //   addCard(data) {
 //     return fetch(`${this._url}/cards`, {
 //       method: "POST",
-//       headers: this._setHeaders(),
+//       headers: this._headers,
 //       body: JSON.stringify({
 //         name: data.name,
 //         link: data.link,
 //       }),
 //     }).then(this._getJson);
 //   }
-
 //   // удалить карточку (DELETE)
 //   deleteCard(id) {
 //     return fetch(`${this._url}/cards/${id}`, {
 //       method: "DELETE",
-//       headers: this._setHeaders(),
+//       headers: this._headers,
 //     }).then(this._getJson);
 //   }
 
-//   // “залайкать” карточку (PUT)
-//   putLike(id) {
-//     return fetch(`${this._url}/cards/${id}/likes`, {
-//       method: "PUT",
-//       headers: this._setHeaders(),
+//   // получить данные пользователя (GET)
+//   getDataUser() {
+//     return fetch(`${this._url}/users/me`, {
+//       // method: "GET",
+//       headers: this._headers,
 //     }).then(this._getJson);
 //   }
-
-//     // удалить лайк карточки (DELETE)
-//     deleteLike(id) {
-//       return fetch(`${this._url}/cards/${id}/likes`, {
-//         method: "DELETE",
-//         headers: this._setHeaders(),
-//       }).then(this._getJson);
-//     }
 
 //   // заменить данные пользователя (PATCH)
 //   editInfoUser(data) {
 //     return fetch(`${this._url}/users/me`, {
 //       method: "PATCH",
-//       headers: this._setHeaders(),
+//       headers: this._headers,
 //       body: JSON.stringify({
 //         name: data.name,
 //         about: data.about,
@@ -205,13 +190,28 @@ export const api = new Api({
 //   editUserAvatar(data) {
 //     return fetch(`${this._url}/users/me/avatar`, {
 //       method: "PATCH",
-//       headers: this._setHeaders(),
+//       headers: this._headers,
 //       body: JSON.stringify({
 //         avatar: data.avatar,
 //       }),
 //     }).then(this._getJson);
 //   }
 
+//   // “залайкать” карточку (PUT)
+//   putLike(id) {
+//     return fetch(`${this._url}/cards/${id}/likes`, {
+//       method: "PUT",
+//       headers: this._headers,
+//     }).then(this._getJson);
+//   }
+
+//   // удалить лайк карточки (DELETE)
+//   deleteLike(id) {
+//     return fetch(`${this._url}/cards/${id}/likes`, {
+//       method: "DELETE",
+//       headers: this._headers,
+//     }).then(this._getJson);
+//   }
 
 //   // для лайка
 //   changeLikeCardStatus(cardId, isLiked) {
@@ -223,13 +223,13 @@ export const api = new Api({
 //   }
 // }
 
-// // export const api = new Api({
-// //   url: 'https://api.pr-mesto.nomoredomains.xyz',
-// //   headers: {
-// //     'Content-Type': 'application/json',
-// //     authorization: `Bearer ${localStorage.getItem('jwt')}`,
-// //   },
-// // });
+// export const api = new Api({
+//   url: 'https://api.pr-mesto.nomoredomains.xyz',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     authorization: `Bearer ${localStorage.getItem('jwt')}`,
+//   },
+// });
 
 // export const api = new Api({
 //   url: "https://mesto.nomoreparties.co/v1/cohort-62",
@@ -238,3 +238,4 @@ export const api = new Api({
 //     authorization: "fff1efa7-9818-44da-ba96-913e90767349",
 //   },
 // });
+
