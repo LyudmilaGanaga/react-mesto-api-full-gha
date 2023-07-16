0478587b4086282a04f93d003cdba4b8ff3822f4
Mila <milanaganaga@yandex.ru>
2023-07-16 22:17:52 +0400

    фикс локал

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -40,7 +40,7 @@ export const checkToken = (token) => {
     headers: {
       'Accept': 'application/json',
       "Content-Type": "application/json",
-       "Authorization": `Bearer ${token}`
+      //  "Authorization": `Bearer ${token}`
     },
   })
     .then((res) => {
