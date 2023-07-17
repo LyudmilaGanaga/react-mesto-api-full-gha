d82997f4403609917f8c296b063caa3bb69f2035
Mila <milanaganaga@yandex.ru>
2023-07-17 15:22:22 +0400

    cors

--- backend/app.js
+++ backend/app.js
@@ -5,19 +5,15 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlwares/logger');
 
-// const cors = require('cors');
-
 const routes = require('./routes');
 const errorHandler = require('./middlwares/error');
+// const cors = require('cors');
+const cors = require('./middlwares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-});
-
-// app.use(cors);
+app.use(cors);
 
 app.use(requestLogger);
 
@@ -34,6 +30,10 @@ app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
---
+++ backend/middlwares/cors.js
@@ -0,0 +1,26 @@
+const allowedCors = [
+  'http://pr-mesto.nomoredomains.work',
+  'https://pr-mesto.nomoredomains.work',
+  'http://api.pr-mesto.nomoredomains.xyz',
+  'https://api.pr-mesto.nomoredomains.xyz',
+  'localhost:3000',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+};
