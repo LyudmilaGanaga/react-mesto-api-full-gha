644d26ef3d1eecbda9698c437c976d4216bb7f21
Mila <milanaganaga@yandex.ru>
2023-07-17 19:42:03 +0400

    AppPlacePopup

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -11,11 +11,12 @@ export default function Card({
   const handleLikeClick = () => onCardLike(card);
   const handleDeleteClick = () => onCardDeleteConfirm(card);
 
-  const currentUser = useContext(CurrentUserContext);
+  // const currentUser = useContext(CurrentUserContext);
+
   // Определяем, являемся ли мы владельцем текущей карточки
-  const isOwn = card.owner === currentUser._id;
+  const isOwn = card.owner._id === CurrentUserContext._id;
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some(i => i._id === CurrentUserContext._id);
   // Создаём переменную, которую после зададим в className для кнопки лайка
   const cardLikeButtonClassName = `element__like ${
     isLiked && "element__like_active"
@@ -34,7 +35,7 @@ export default function Card({
         <img
           className="element__image"
           src={card.link}
-          alt={card.name}
+          alt={`Картинка ${card.name}`} 
           onClick={handleClick}
         />
         <div className="element__group">
