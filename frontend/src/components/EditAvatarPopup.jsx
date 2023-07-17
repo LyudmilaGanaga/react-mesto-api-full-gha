c5c3da5379001f8bc4b670d2f61ca9899c8aebc3
Mila <milanaganaga@yandex.ru>
2023-07-17 18:44:10 +0400

    Avatar

--- frontend/src/components/EditAvatarPopup.jsx
+++ frontend/src/components/EditAvatarPopup.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import PopupWithForm from "./PopupWithForm";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 import { useForm } from "react-hook-form";
 import classNames from "classnames";
 
-export default function EditAvatarPopup({ onUpdateAvatar, isOpen, onClose }) {
-
-  const avatarRef = useRef();
-  
+export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
+  const currentUser = useContext(CurrentUserContext);
   const {
+    register,
     handleSubmit,
     reset,
     formState: { errors, isValid, isDirty },
@@ -16,9 +16,11 @@ export default function EditAvatarPopup({ onUpdateAvatar, isOpen, onClose }) {
     mode: "onChange",
   });
 
+  const avatarRef = useRef();
+
   useEffect(() => {
     reset();
-    avatarRef.current.value = "";
+    // avatarRef.current.value = "";
   }, [isOpen, reset]);
 
   function onSubmit({ avatar }) {
@@ -42,14 +44,14 @@ export default function EditAvatarPopup({ onUpdateAvatar, isOpen, onClose }) {
         })}
         type="url"
         placeholder="Ссылка на картинку"
-        // {...register("avatar", {
-        //   required: "Пожалуйста введите URL",
-        //   pattern: {
-        //     value:
-        //       /^(http|https)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(:[0-9]{2,5})?(\/[a-zA-Z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~]*)*$/,
-        //     message: "Введите адрес сайта",
-        //   },
-        // })}
+        {...register("avatar", {
+          required: "Пожалуйста введите URL",
+          pattern: {
+            // value:
+            //   /^(http|https)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(:[0-9]{2,5})?(\/[a-zA-Z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~]*)*$/,
+            message: "Введите адрес сайта",
+          },
+        })}
       />
       <span
         className={classNames("popup__input-error", {
