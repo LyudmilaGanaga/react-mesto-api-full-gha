fd5252dbd92f58975e37db466aee5373274e67f8
Mila <milanaganaga@yandex.ru>
2023-07-17 14:52:10 +0400

    jwt

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,6 +1,5 @@
-
 class Api {
-  constructor( url, headers ) {
+  constructor({ url, headers }) {
     this._url = url;
     this._headers = headers;
   }
@@ -13,7 +12,7 @@ class Api {
       return Promise.reject(`Ошибка: ${res.status}`);
     }
   }
-
+// заголовки
   _setHeaders () {
     const token = localStorage.getItem('token');
     return {
@@ -22,10 +21,9 @@ class Api {
     };
   }
   
-  // получить список всех карточек в виде массива (GET)
+// получить список всех карточек в виде массива (GET) 
   getInitialCards() {
     return fetch(`${this._url}/cards`, {
-      // method: "GET",
       headers: this._setHeaders(),
     }).then(this._getJson);
   }
@@ -33,7 +31,6 @@ class Api {
 // получить данные пользователя (GET)
 getDataUser() {
   return fetch(`${this._url}/users/me`, {
-    // method: "GET",
     headers: this._setHeaders(),
   }).then(this._getJson);
 }
@@ -67,7 +64,7 @@ getDataUser() {
   }
 
     // удалить лайк карточки (DELETE)
-    deleteLike(id) {
+    deleteLike(id, token) {
       return fetch(`${this._url}/cards/${id}/likes`, {
         method: "DELETE",
         headers: this._setHeaders(),
@@ -97,7 +94,6 @@ getDataUser() {
     }).then(this._getJson);
   }
 
-
   // для лайка
   changeLikeCardStatus(cardId, isLiked) {
     if (isLiked) {
@@ -109,119 +105,12 @@ getDataUser() {
 }
 
 export const api = new Api({
-  url: 'https://api.pr-mesto.nomoredomains.xyz',
+  fetchUrl: 'https://api.pr-mesto.nomoredomains.xyz',
   headers: {
-    'Content-Type': 'application/json',
-    authorization: `Bearer ${localStorage.getItem('jwt')}`,
-  },
-});
-
-// export const api = new Api({
-//   url: "https://mesto.nomoreparties.co/v1/cohort-62",
-//   headers: {
-//     "Content-Type": "application/json",
-//     authorization: "fff1efa7-9818-44da-ba96-913e90767349",
-//   },
-// });
-
-
-// class Api {
-//   constructor(config) {
-//     this._url = config.url;
-//     this._headers = config.headers;
-//   }
-
-//   // проверка
-//   _getJson(res) {
-//     if (res.ok) {
-//       return res.json();
-//     } else {
-//       return Promise.reject(`Ошибка: ${res.status}`);
-//     }
-//   }
-//   // получить список всех карточек в виде массива (GET)
-//   getInitialCards() {
-//     return fetch(`${this._url}/cards`, {
-//       method: "GET",
-//       headers: this._headers,
-//     }).then(this._getJson);
-//   }
-
-//   // добавить карточку (POST)
-//   addCard(data) {
-//     return fetch(`${this._url}/cards`, {
-//       method: "POST",
-//       headers: this._headers,
-//       body: JSON.stringify({
-//         name: data.name,
-//         link: data.link,
-//       }),
-//     }).then(this._getJson);
-//   }
-//   // удалить карточку (DELETE)
-//   deleteCard(id) {
-//     return fetch(`${this._url}/cards/${id}`, {
-//       method: "DELETE",
-//       headers: this._headers,
-//     }).then(this._getJson);
-//   }
-
-//   // получить данные пользователя (GET)
-//   getDataUser() {
-//     return fetch(`${this._url}/users/me`, {
-//       // method: "GET",
-//       headers: this._headers,
-//     }).then(this._getJson);
-//   }
-
-//   // заменить данные пользователя (PATCH)
-//   editInfoUser(data) {
-//     return fetch(`${this._url}/users/me`, {
-//       method: "PATCH",
-//       headers: this._headers,
-//       body: JSON.stringify({
-//         name: data.name,
-//         about: data.about,
-//       }),
-//     }).then(this._getJson);
-//   }
-
-//   // заменить аватар (PATCH)
-//   editUserAvatar(data) {
-//     return fetch(`${this._url}/users/me/avatar`, {
-//       method: "PATCH",
-//       headers: this._headers,
-//       body: JSON.stringify({
-//         avatar: data.avatar,
-//       }),
-//     }).then(this._getJson);
-//   }
-
-//   // “залайкать” карточку (PUT)
-//   putLike(id) {
-//     return fetch(`${this._url}/cards/${id}/likes`, {
-//       method: "PUT",
-//       headers: this._headers,
-//     }).then(this._getJson);
-//   }
-
-//   // удалить лайк карточки (DELETE)
-//   deleteLike(id) {
-//     return fetch(`${this._url}/cards/${id}/likes`, {
-//       method: "DELETE",
-//       headers: this._headers,
-//     }).then(this._getJson);
-//   }
-
-//   // для лайка
-//   changeLikeCardStatus(cardId, isLiked) {
-//     if (isLiked) {
-//       return this.putLike(cardId);
-//     } else {
-//       return this.deleteLike(cardId);
-//     }
-//   }
-// }
+  'Accept': 'application/json',
+  'Content-type': 'application/json',
+  }
+})
 
 // export const api = new Api({
 //   url: 'https://api.pr-mesto.nomoredomains.xyz',
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,49 +1,48 @@
 export const BASE_URL = "https://api.pr-mesto.nomoredomains.xyz";
 
-export const checkServer = (res) => {
+function checkServer(res) {
   if (res.ok) {
     return res.json();
-  }else{
-  return Promise.reject(`Ошибка: ${res.status}`);
+  } else {
+    return Promise.reject(`Ошибка: ${res.status}`);
   }
-};
+}
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({email, password}),
+    body: JSON.stringify({ email, password }),
   }).then((res) => {
-    return checkServer(res)
-  })
-}
+    return checkServer(res);
+  });
+};
 
 export const login = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
   }).then((res) => {
     return checkServer(res);
-})
-}
+  });
+};
 
 export const checkToken = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: "GET",
     headers: {
-      'Accept': 'application/json',
+      Accept: "application/json",
       "Content-Type": "application/json",
-       "Authorization": `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
-  })
-    .then((res) => {
-      return checkServer(res)
-})
+  }).then((res) => {
+    return checkServer(res);
+  });
 };
